a64fcb05ffa425d6d3f43e147d970d79f826b37e
guga56 <gustavogregorio086@gmail.com>
2023-07-19 13:10:41 -0300

    desenvolvimento de CRUD para sistema de contatos

---
+++ ControleDeContatos/ControleDeContatos/Models/ContatoModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeContatos.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Digite o Nome do Contato")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o E-mail do Contato")]
+        [EmailAddress(ErrorMessage = "O email informado não é valido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Digite o Celular do Contato")]
+        [Phone(ErrorMessage = "O celular informado não é valido")]
+        public string Celular { get; set; }
+    }
+}
